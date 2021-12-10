e3994d8571a662c8b24ee5af62969d6d1f2e0397
Roberto J. Cerrud <rcerrud@mire.gob.pa>
2021-12-10 17:34:28 -0500

    Clases de Modelos de Datos y inicio de componentes

---
+++ BlazorApp_WASM/Modelos/Matriz_Reciprocidad.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp_WASM.Modelos
+{
+    public class Reciprocidad
+    {
+        public int codigo_tipo_tre { get; set; }
+        public int id_emcori { get; set; }
+    }
+
+    public class Matriz_Reciprocidad
+    {
+        public List<Reciprocidad>? matriz { get; set; }
+    }
+}
---
+++ BlazorApp_WASM/Modelos/Tipos_Tre_Habilitados.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp_WASM.Modelos
+{
+    
+    public class Tre_Habilitado
+    {
+        public int codigo_tipo_tre { get; set; }
+        public string? descripcion { get; set; }
+        public bool habilitado { get; set; }
+    
+    }
+    
+    public class Tipos_Tre_Habilitados
+    {
+        public List<Tre_Habilitado>? tre_habilitados { get; set; } 
+    }
+}
